c7d3366421c061936ffc9bb1256f0a6214326646
main <main@MAIN-PC>
2024-03-16 19:23:28 +0900

    Modify to be able to test with H2database

--- src/main/java/com/skywhalelab/SpringBootJpa/cmmUtil.java
+++ src/main/java/com/skywhalelab/SpringBootJpa/cmmUtil.java
@@ -2,10 +2,15 @@ package com.skywhalelab.SpringBootJpa;
 
 public final class cmmUtil {
 	
+//	oracle
 //	public static final String DB_VARCHAR = "VARCHAR2";
 //	public static final String DB_SYSDATE = "SYSDATE";
 	
+//	mysql or mariadb
+//	public static final String DB_VARCHAR = "varchar";
+//	public static final String DB_SYSDATE = "sysdate()";
+	
 	public static final String DB_VARCHAR = "varchar";
-	public static final String DB_SYSDATE = "sysdate()";
+	public static final String DB_SYSDATE = "current_timestamp()";
 	
 }
